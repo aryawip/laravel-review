14a2185e23fa6fd1d402a75048abff550704827b
Arya Wira P <awp.aryawira@gmail.com>
2022-09-13 11:12:04 +0700

    membuat halaman login dan register

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,5 +49,11 @@ Route::get('/about', function () {
         "email" => "aryawira@gmail.com"
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+
 
 
