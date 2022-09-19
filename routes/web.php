d276879d0a43edc0c22079f60cbfb0cde9033cad
Arya Wira P <awp.aryawira@gmail.com>
2022-09-19 09:05:39 +0700

    autentikasi untuk login

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::get('/about', function () {
 
 Route::get('/login', [LoginController::class, 'index']);
 
+Route::post('/login', [LoginController::class, 'authenticate']);
+
 Route::get('/register', [RegisterController::class, 'index']);
 
 Route::post('/register', [RegisterController::class, 'store']);
