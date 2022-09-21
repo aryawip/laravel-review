a0375f1a60605ff1995527984b8d09396aa8e441
Arya Wira P <awp.aryawira@gmail.com>
2022-09-21 10:48:13 +0700

    menambah preview gambar

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -45,9 +45,10 @@
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Post Image</label>
+                <img class="img-preview img-fluid mb-3 col-sm-5">
                 <input class="form-control @error('image')
                     is-invalid
-                @enderror" type="file" id="image" name="image">
+                @enderror" type="file" id="image" name="image" onchange="previewImage()">
                 @error('image')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -79,5 +80,22 @@
         document.addEventListener('trix-file-accept', function(e) {
             e.preventDefault();
         });
+
+        function previewImage() {
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+
+            imgPreview.style.display = 'block';
+
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+
+            oFReader.onload = function(oFREvent) {
+                imgPreview.src = oFREvent.target.result;
+            
+            // const oFReader = URL.createObjectURL(image.files[0]);
+            //     imgPreview.src = oFReader;
+            }
+        }
     </script>
 @endsection
